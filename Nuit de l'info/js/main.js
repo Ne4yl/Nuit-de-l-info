310ac49a1bfd153383fab5798fc356cf679b2aaa
KBM106 <96017088+kbm-106@users.noreply.github.com>
2023-12-08 04:18:51 +0100

    Add files via upload

--- Nuit de l'info/js/main.js
+++ Nuit de l'info/js/main.js
@@ -4,3 +4,18 @@ let docTitle = document.title; window.addEventListener("blur", () => { document.
 });
 window.addEventListener("focus", () => { document.title = docTitle;
 });
+
+/*ahhahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
+
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  
+  document.querySelectorAll(".cloud").forEach(function(move){
+
+    var moving_value = move.getAttribute("data-value");
+    var x = (e.clientX * moving_value) / 250;
+    var y = (e.clientY * moving_value) / 250;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
