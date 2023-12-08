fe5b0d6a4eb44b2df5d0766020a032c048367fe5
Ne4yl <noann.maza@gmail.Com>
2023-12-08 07:34:59 +0100

    Final smash

--- Nuit de l'info/Smash_bross/script.js
+++ Nuit de l'info/Smash_bross/script.js
@@ -31,20 +31,40 @@ const projectile = {
 }
 
 function attaque(){
+    x = 0;
     console.log('Attaque!');
     console.log((player.x + player_default.largeur));
     const projectileDiv = document.createElement('img');
-    projectileDiv.src = 'Mario_Fireball.png';
+    projectileDiv.src = 'projectile.png';
     projectileDiv.className = 'projectile';
-    projectileDiv.style.lef = (player.x + player_default.largeur) + 'px';
-    projectileDiv.style.top = player.y + 'px';
-    projectileDiv.style.width = 150 + 'px';
-    projectileDiv.style.height = 146 + 'px';
+    projectileDiv.style.left = (player.x + player_default.largeur) + 'px';
+    projectileDiv.style.top = 400 + 'px';
+    projectileDiv.style.width = 100 + 'px';
+    projectileDiv.style.height = 164 + 'px';
     projectileDiv.style.height = projectile.largeur + 'px';
     projectileDiv.style.width = projectile.hauteur + 'px';
     document.querySelector('.bullet').appendChild(projectileDiv);
     sleep(1000).then(() => {
-        document.querySelector('.bullet').remove();
+        document.querySelector('.projectile').remove();
+    });
+}
+
+function attaque_enemy(){
+    x = 0;
+    console.log('Attaque Enemy !');
+    console.log((player.x + player_default.largeur));
+    const projectileDiv = document.createElement('img');
+    projectileDiv.src = 'fusee.png';
+    projectileDiv.className = 'projectile';
+    projectileDiv.style.left = (enemy.x + player_default.largeur) + 'px';
+    projectileDiv.style.top = 400 + 'px';
+    projectileDiv.style.width = 100 + 'px';
+    projectileDiv.style.height = 164 + 'px';
+    projectileDiv.style.height = projectile.largeur + 'px';
+    projectileDiv.style.width = projectile.hauteur + 'px';
+    document.querySelector('.bullet').appendChild(projectileDiv);
+    sleep(1000).then(() => {
+        document.querySelector('.projectile').remove();
     });
 }
 
@@ -88,54 +108,58 @@ addEventListener("keydown", function (e) {
 
     if (e.code === 'Space') {
         // Right
+        console.log('Space');
         attaque();
     }
 });
 
 
 // Enemy 
-// var enemyDiv = document.getElementById('enemy');
+var enemyDiv = document.getElementById('enemy');
 
-// const enemy_default = {
-//     x: rect.right,
-//     y: rect.top,
-//     largeur : rect.width,
-//     hauteur : rect.height,
-// }
+const enemy_default = {
+    x: rect.right,
+    y: rect.top,
+    largeur : rect.width,
+    hauteur : rect.height,
+}
 
-// var enemy = {
-//     x: enemy_default.x,
-//     y: enemy_default.y,
-// }
+var enemy = {
+    x: enemy_default.x,
+    y: enemy_default.y,
+}
 
-// var enemySpeed = 128.6666; // Ajuste la vitesse de l'ennemi selon tes besoins
+var enemySpeed = 128.6666; // Ajuste la vitesse de l'ennemi selon tes besoins
 
-// function updateEnemyPosition() {
-//     // Déplacement de l'ennemi vers le joueur (simplement en suivant l'axe X dans cet exemple)
-//     console.log('enemy.x : ' + enemy.x);
-//     if (enemy.x > 0 && enemy.x < player.x) {
-//         enemy.x += enemySpeed;
-//     } 
-//     if (enemy.x > player.x && enemy.x < 1136){
-//             enemy.x -= enemySpeed;
-//     }
-//     console.log('enemy.x : ' + enemy.x);
+function updateEnemyPosition() {
+    // Déplacement de l'ennemi vers le joueur (simplement en suivant l'axe X dans cet exemple)
+    console.log('enemy.x : ' + enemy.x);
+    if (enemy.x < player.x) {
+        enemy.x += enemySpeed;
+    } 
+    if (enemy.x > player.x){
+            enemy.x -= enemySpeed;
+    }
+    sleep(1000).then(() => {attaque_enemy();});
+    
+    console.log('enemy.x : ' + enemy.x);
 
-//     // Met à jour la position de l'ennemi dans le DOM
-//     enemyDiv.style.left = enemy.x + 'px';
-//     enemyDiv.style.top = enemy.y + 'px';
 
-//     // Vérifie si l'ennemi touche le joueur
-//     if (
-//         enemy.x < player.x + player.width &&
-//         enemy.x + enemy.width > player.x &&
-//         enemy.y < player.y + player.height &&
-//         enemy.y + enemy.height > player.y
-//     ) {
-//         // Le joueur est touché, tu peux ajouter ici le code correspondant à ce scénario
-//         console.log('Le joueur est touché!');
-//     }
-// }
+    // Met à jour la position de l'ennemi dans le DOM
+    enemyDiv.style.left = enemy.x + 'px';
+    enemyDiv.style.top = enemy.y + 'px';
 
-// // Appelle la fonction de mise à jour de la position de l'ennemi à intervalles réguliers
-// setInterval(updateEnemyPosition, 500);
+    // Vérifie si l'ennemi touche le joueur
+    if (
+        enemy.x < player.x + player.width &&
+        enemy.x + enemy.width > player.x &&
+        enemy.y < player.y + player.height &&
+        enemy.y + enemy.height > player.y
+    ) {
+        // Le joueur est touché, tu peux ajouter ici le code correspondant à ce scénario
+        console.log('Le joueur est touché!');
+    }
+}
+
+// Appelle la fonction de mise à jour de la position de l'ennemi à intervalles réguliers
+setInterval(updateEnemyPosition, 500);
